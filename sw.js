5fcd564a3bccd8d73f5bbb153d86e6f374171b1c
Carm <cvalera@sixgill.com>
2018-09-13 13:20:30 -0400

    Fix extraneous heading for Templates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-dd35e7b3e59a567601eb.js","a4c687d8590659093c4e03c53127c817"],["/commons-2ad7743a8fdb6a4020fa.js","661ff7293331f65b5e7b858324101614"],["/index.html","b898979cdc86f0e1c06d2eb2e2d7d0cf"],["/offline-plugin-app-shell-fallback/index.html","5e208c97a9832b6b12b51fed812e48e6"]];
+var precacheConfig = [["/app-dd35e7b3e59a567601eb.js","a4c687d8590659093c4e03c53127c817"],["/commons-2ad7743a8fdb6a4020fa.js","661ff7293331f65b5e7b858324101614"],["/index.html","9a15b26ebe6c5918a68be125fdeb4adf"],["/offline-plugin-app-shell-fallback/index.html","95d6deecf84dd84bc25705231eb41f60"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
